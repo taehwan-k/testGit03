ea64c7b035bbea63b561162906735be49e0933cb
hk_edu <hk_edu@192.168.2.106>
2020-04-28 11:05:00 +0900

    ㅇㄹ

--- testGitProject/src/com/hk/git/GitTest.java
+++ testGitProject/src/com/hk/git/GitTest.java
@@ -3,34 +3,6 @@ package com.hk.git;
 public class GitTest {
 
 	public static void main(String[] args) {
-
-
-		System.out.println("ㅎㅎㅎㅎㅎㅎㅎ");
-
-
-		System.out.println("ㅎㅎㅎㅎㅎㅎㅎ");
-
-
-		System.out.println("ㅎㅎㅎㅎㅎㅎㅎ");
-
-
-		System.out.println("ㅎㅎㅎㅎㅎㅎㅎ");
-
-
-		System.out.println("ㅎㅎㅎㅎㅎㅎㅎ");
-
-
-		System.out.println("ㅎㅎㅎㅎㅎㅎㅎ");
-
-
-		System.out.println("ㅎㅎㅎㅎㅎㅎㅎ");
-		
-		System.out.println("안녕?");
-		System.out.println("안녕?");
-		System.out.println("안녕?");
-		System.out.println("안녕?");
-		System.out.println("안녕?");
-		System.out.println("안녕?");
-		System.out.println("안녕?");
+		System.out.println("ㅇㅇㅇㅇㅇㅇㅇ");
 	}
 }
