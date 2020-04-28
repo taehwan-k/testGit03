a7c323ab1be2694d8ccc062084c9f1e65e5be266
hkedu <hkedu@192.168.2.108>
2020-04-28 10:32:41 +0900

    윤호

--- testGitProject/src/com/hk/git/GitTest.java
+++ testGitProject/src/com/hk/git/GitTest.java
@@ -3,14 +3,6 @@ package com.hk.git;
 public class GitTest {
 
 	public static void main(String[] args) {
-
-		System.out.println("추가작업");
-		
-		System.out.println("태환작업 랄허이리항ㄹ히ㅏ");
-
-		System.out.println("강쌤이 작업함");
-	
-		System.out.println("강쌤이 작업함ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ");
-
+		System.out.println("윤호작업");
 	}
 }
