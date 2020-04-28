bf77468cac7979b95260c2683ec7f8db90e96fc5
hk_edu <hk_edu@192.168.2.106>
2020-04-28 11:09:39 +0900

    ㄹㅇㄹㅇ

--- testGitProject/src/com/hk/git/GitTest.java
+++ testGitProject/src/com/hk/git/GitTest.java
@@ -3,14 +3,16 @@ package com.hk.git;
 public class GitTest {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 
 
 		System.out.println("ㅎㅎㅎㅎㅎㅎㅎ");
 		System.out.println("ㅎㅎㅎㅎㅎㅎㅎ");
 
-=======
+
 		System.out.println("ㅇㅇㅇㅇㅇㅇㅇ");
->>>>>>> branch 'master' of https://github.com/taehwan-k/testGit03.git
+		
+		System.out.println("ㄴㄴㄴㄴㄴㄴㄴㄴㄴ");
+
 	}
 }
