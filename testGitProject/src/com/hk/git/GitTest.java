0861782bceb18e6b53ea958ff391f871a56d8445
user <user@DESKTOP-AUU34GB>
2020-04-28 10:16:55 +0900

    수정

--- testGitProject/src/com/hk/git/GitTest.java
+++ testGitProject/src/com/hk/git/GitTest.java
@@ -4,9 +4,11 @@ public class GitTest {
 
 	public static void main(String[] args) {
 
+		System.out.println("추가작업");
+		
 		System.out.println("태환작업 랄허이리항ㄹ히ㅏ");
 
 		System.out.println("강쌤이 작업함");
-
+		
 	}
 }
