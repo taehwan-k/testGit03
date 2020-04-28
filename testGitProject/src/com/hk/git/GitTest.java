4237b610c86cfac9444db84644e834211cdfa05f
hkedu <hkedu@192.168.2.108>
2020-04-28 10:57:28 +0900

    ㄴㅁ

--- testGitProject/src/com/hk/git/GitTest.java
+++ testGitProject/src/com/hk/git/GitTest.java
@@ -3,19 +3,6 @@ package com.hk.git;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("윤호작업");
-		System.out.println("윤호작업호호호");
-		System.out.println("태환작업호호호");
-		System.out.println("ffff");
-
-		System.out.println("fffㅎㅇㅎㄹㅇㅎf");
-		System.out.println("태환이가 수정 후 커밋안함");
-
-		System.out.println("fffㅎㅇㅎㄹㅇㅎf");
-		System.out.println("ㅎㅇㅎㅇ");
-		System.out.println("ㅎㅇㅎㅇ");
-		System.out.println("ㅎㅇㅎㅇ");
-		System.out.println("ㅎㅇㅎㅇ");
 
 
 		System.out.println("ㅎㅎㅎㅎㅎㅎㅎ");
